707d91603f4bb9347ba58e30ce71e31419950d17
moses-Desktop <moseschanx@gmail.com>
2021-08-10 00:21:10 +0800

    modifed file

--- Liquid Display Module (Hitachi HD44780)/config.h
+++ Liquid Display Module (Hitachi HD44780)/config.h
@@ -3,11 +3,16 @@
 
 #include <reg51.h>
 
+
+/***** Connection *****/
+
 sbit  LCM_E =  P3^5;
 sbit  LCM_RW = P3^6;
 sbit  LCM_RS = P3^7;
 
 #define LCM_DB	P0
+
+/***** Connection *****/
 
 sbit LCM_DB6 = LCM_DB^6;
 sbit LCM_DB7 = LCM_DB^7;
